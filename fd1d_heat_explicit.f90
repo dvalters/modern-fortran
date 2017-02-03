625f22425911394c8426e3eb0d70342920440746
Declan Valters <declan.valters@manchester.ac.uk>
2017-02-03 16:43:05 +0000

    CFL module created

--- fd1d_heat_explicit.f90
+++ fd1d_heat_explicit.f90
@@ -138,30 +138,6 @@ program fd1d_heat_explicit_prb
       h_new(x_num) = 70.0_DP
     end subroutine fd1d_heat_explicit
 
-    subroutine fd1d_heat_explicit_cfl( k, t_num, t_min, t_max, x_num, x_min, x_max, cfl )
-
-      implicit none
-
-      real(kind=DP), intent(inout) :: cfl
-      real(kind=DP) :: dx
-      real(kind=DP) :: dt
-      real(kind=DP), intent(in) :: k
-      real(kind=DP), intent(in) :: t_max
-      real(kind=DP), intent(in) :: t_min
-      integer(kind=SI), intent(in) :: t_num
-      real(kind=DP), intent(in) :: x_max
-      real(kind=DP), intent(in) :: x_min
-      integer(kind=SI), intent(in) :: x_num
-
-      dx = ( x_max - x_min ) / dble( x_num - 1 )
-      dt = ( t_max - t_min ) / dble( t_num - 1 )
-
-      cfl = k * dt / dx / dx
-
-      write ( *, '(a)' ) ' '
-      write ( *, '(a,g14.6)' ) '  CFL stability criterion value = ', cfl
-
-    end subroutine fd1d_heat_explicit_cfl
 
     subroutine r8mat_write( output_filename, m, n, table )
       implicit none
