aff7c5eeb3ef210f6e86ff37314eee5e4f92ea91
Declan Valters <declan.valters@manchester.ac.uk>
2017-02-03 09:54:56 +0000

    Added the types module

---
+++ Types_mod.f90
@@ -0,0 +1,11 @@
+module Types_mod
+        use, intrinsic :: iso_fortran_env
+        implicit none
+        public :: SP
+        integer, parameter :: SP = REAL32
+        integer, parameter :: DP = REAL64
+        integer, parameter :: SI = INT32
+        integer, parameter :: DI = INT64
+contains
+
+end module Types_mod
